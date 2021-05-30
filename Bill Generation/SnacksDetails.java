4390ea549276e1720bc5858ad797f433f07590b8
Aakash Pawan G P S <70209094+AakashPawanGPS@users.noreply.github.com>
2021-05-30 23:39:34 +0530

    Add files via upload
    Problems For Practice

---
+++ Bill Generation/SnacksDetails.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class SnacksDetails{
+    public static void main(String[] args){
+        String obj = "Only a life lived for others is a life worthwhile";
+
+        System.out.println(obj.length());
+    }
+}
